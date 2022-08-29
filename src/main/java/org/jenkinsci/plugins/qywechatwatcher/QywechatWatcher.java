6ce11e242b0af33222fb4975a9f68b188417451a
bright.ma <bright.ma@blackshark.com>
2022-08-29 15:59:56 +0800

    src: java: add get getMentionedList getMobileList to QywechatWatcher

--- src/main/java/org/jenkinsci/plugins/qywechatwatcher/QywechatWatcher.java
+++ src/main/java/org/jenkinsci/plugins/qywechatwatcher/QywechatWatcher.java
@@ -8,6 +8,7 @@ import hudson.model.User;
 import hudson.plugins.jobConfigHistory.JobConfigHistory;
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
@@ -15,6 +16,8 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.jenkinsci.plugins.qywechatwatcher.jobConfigHistory.ConfigHistory;
+import com.google.common.base.Splitter;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
@@ -25,8 +28,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class QywechatWatcher {
@@ -71,14 +73,17 @@ public class QywechatWatcher {
     public String send(final QywechatWatcherNotification notification) throws MessagingException, AddressException {
         //if (!notification.shouldNotify()) return null; 这里判断 收件人 是否是空了，是空就不通知了。
         String webhookurl = notification.getWebhookurl();
-        String mention = notification.getRecipients();
+
         String[] urls;
         if (webhookurl.contains(",")) {
             urls = webhookurl.split(",");
         } else {
             urls = new String[]{webhookurl};
         }
-        if (urls.length == 0) return null;
+        if (urls.length == 0) {
+            LOGGER.warning("webhookurl is null");
+            return null;
+        }
 
         String data = toJSONString(notification);
 
@@ -97,18 +102,54 @@ public class QywechatWatcher {
 
     private String toJSONString(final QywechatWatcherNotification notification) {
         //组装内容
+        String mention = notification.getRecipients();
+        List<String> mentionedList = getMentionedList(mention);
+        List<String> mobileList = getMobileList(mention);
+
         StringBuilder content = new StringBuilder();
         content.append("<font color=\"info\">" + notification.getMailSubject() + "</font>\n");
         content.append(notification.getMailBody());
-        Map markdown = new HashMap<String, Object>();
-        markdown.put("content", content.toString());
+
+        Map mdMap = new HashMap<String, Object>();
+        mdMap.put("content", content.toString());
+
+        if(!CollectionUtils.isEmpty(mentionedList))
+            mdMap.put("mentioned_list", mentionedList);
+        if(!CollectionUtils.isEmpty(mobileList))
+            mdMap.put("mentioned_mobile_list", mobileList);
 
         Map data = new HashMap<String, Object>();
         data.put("msgtype", "markdown");
-        data.put("markdown", markdown);
+        data.put("markdown", mdMap);
 
         String req = JSONObject.fromObject(data).toString();
         return req;
+    }
+
+    private List<String> getMentionedList(String mention) { // # 用户 userID 列表
+        List<String> list = new ArrayList<>();
+        if (StringUtils.isNotEmpty(mention)) {
+            Iterable<String> iterable = Splitter.on(',').omitEmptyStrings().split(mention);
+            for (String result : iterable) {
+                if (mention.startsWith("qy") || mention.equals("@all")) {
+                    list.add(result);
+                }
+            }
+        }
+        return list;
+    }
+
+    private List<String> getMobileList(String mention) { // # 用户 mobile 列表
+        List<String> list = new ArrayList<>();
+        if (StringUtils.isNotEmpty(mention)) {
+            Iterable<String> iterable = Splitter.on(',').omitEmptyStrings().split(mention);
+            for (String result : iterable) {
+                if (!mention.startsWith("qy") && !mention.equals("@all")) {
+                    list.add(result);
+                }
+            }
+        }
+        return list;
     }
 
     private static String push(String url, String data) throws HttpProcessException, KeyManagementException, NoSuchAlgorithmException {
